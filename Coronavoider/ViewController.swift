572cd97543255d45f1153bdfc836c9b67be21f26
Taveeh <octavian.custura@gmail.com>
2020-11-07 14:03:43 +0200

    Initial Commit

---
+++ Coronavoider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coronavoider
+//
+//  Created by Octavian Custura on 07/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
